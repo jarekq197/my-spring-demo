c83a1ebbf18dc7f3fc192bb64b1e198409ebfea9
Jarek <jarekq197@gmail.com>
2018-12-14 11:54:29 +0100

    Add first commit

---
+++ src/main/java/pl/mpas/myspringdemo/MySpringDemoApplication.java
@@ -0,0 +1,14 @@
+package pl.mpas.myspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringDemoApplication.class, args);
+    }
+
+}
+
